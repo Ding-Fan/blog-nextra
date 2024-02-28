75f8c498a482d8bbf87121038756f1c579c1aadd
Ding Fan <ouud@fastmail.com>
2024-02-29 04:28:54 +0900

    :lipstick: Font swap

--- components/RichImage.tsx
+++ components/RichImage.tsx
@@ -5,24 +5,35 @@ import styles from "./RichImage.module.css";
 interface RichImageProps extends BaseImageProps {
   effect?: "blur" | "hoverClear";
   caption?: ReactNode;
+  align?: "center" | "left";
 }
 
-const RichImage = (props: RichImageProps) => {
+const RichImage = ({
+  effect,
+  caption,
+  align = "center",
+  ...props
+}: RichImageProps) => {
   let theImage = <BaseImage {...props} />;
 
-  if (props.effect === "blur") {
+  if (effect === "blur") {
     theImage = <BaseImage {...props} className={styles.blur} />;
-  } else if (props.effect === "hoverClear") {
+  } else if (effect === "hoverClear") {
     theImage = <BaseImage {...props} className={styles.hoverClear} />;
   }
 
+  let alignClass = "";
+  if (align === "center") {
+    alignClass = "flex flex-col items-center";
+  } else if (align === "left") {
+    alignClass = "flex flex-col items-start";
+  }
+
   return (
-    <div className="flex flex-col items-center">
+    <div className={alignClass}>
       {theImage}
-      {props.caption && (
-        <div className="mt-2 text-xs text-gray-400 text-center">
-          {props.caption}
-        </div>
+      {caption && (
+        <div className="mt-2 text-xs text-gray-400 text-center">{caption}</div>
       )}
     </div>
   );
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>又不是不能写博客</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Do not shot this.",
   },
-}
+};
 
-export default config
+export default config;
