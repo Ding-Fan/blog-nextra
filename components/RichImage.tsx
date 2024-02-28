730943004a5171bc8c568a88082337d548eb523e
Ding Fan <ouud@fastmail.com>
2024-02-29 04:04:39 +0900

    :bento: Font!

--- components/RichImage.tsx
+++ components/RichImage.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import BaseImage, { BaseImageProps } from "./BaseImage";
 import styles from "./RichImage.module.css";
 
 interface RichImageProps extends BaseImageProps {
   effect?: "blur" | "hoverClear";
+  caption?: ReactNode;
 }
 
 const RichImage = (props: RichImageProps) => {
+  let theImage = <BaseImage {...props} />;
+
   if (props.effect === "blur") {
-    return <BaseImage {...props} className={styles.blur} />;
+    theImage = <BaseImage {...props} className={styles.blur} />;
   } else if (props.effect === "hoverClear") {
-    return <BaseImage {...props} className={styles.hoverClear} />;
+    theImage = <BaseImage {...props} className={styles.hoverClear} />;
   }
 
-  return <BaseImage {...props} />;
+  return (
+    <div className="flex flex-col items-center">
+      {theImage}
+      {props.caption && (
+        <div className="mt-2 text-xs text-gray-400 text-center">
+          {props.caption}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RichImage;
