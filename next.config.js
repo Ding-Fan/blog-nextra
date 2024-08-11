71a76a528aeb857fa82a575a3f9ea61212ba812c
Ding Fan <ouud@fastmail.com>
2024-08-11 17:50:22 +0900

    feat: Enable LaTeX support in next.config.js and update music resources

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   standalone: true,
+  latex: true,
 });
 
 module.exports = withNextra();
