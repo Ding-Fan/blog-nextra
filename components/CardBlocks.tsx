b374e5a916dc41306e83ff910c5255b00313b71a
Ding Fan <ouud@fastmail.com>
2024-09-14 18:38:00 +0900

    update

--- components/CardBlocks.tsx
+++ components/CardBlocks.tsx
@@ -94,13 +94,106 @@ const CARD_DATA = [
     id: "c8dfa95e-be5c-4046-876f-f19ab0d95a84",
     content: "throw out trash",
     startTime: dayjs(),
+    category: "chores",
     interval: dayjs.duration(3, "days")
   },
   {
     id: "9355a974-a638-4074-ba59-1f074027bbc9",
     content: "do laundry",
     startTime: dayjs(),
+    category: "chores",
     interval: dayjs.duration(4, "days")
+  },
+  {
+    id: "8d59a612-05d5-4e85-8c33-55ec40a3eb14",
+    content: "do dishes",
+    startTime: dayjs(),
+    category: "chores",
+    interval: dayjs.duration(1, "days")
+  },
+  {
+    id: "11de397f-3a40-41dc-944a-6c8af4d16780",
+    content: "tidy up",
+    startTime: dayjs(),
+    category: "chores",
+    interval: dayjs.duration(4, "days")
+  },
+  {
+    id: "1d43a065-25a3-459f-a42e-4f090c29719d",
+    content: "clean room",
+    startTime: dayjs(),
+    category: "chores",
+    interval: dayjs.duration(2, "days")
+  },
+  {
+    id: "305baca7-e081-4a52-8458-f920fb0a82f7",
+    content: "read book",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(1, "days")
+  },
+  {
+    id: "c5310b27-de13-4e69-9717-818b5e09ed64",
+    content: "organize handouts",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(1, "days")
+  },
+  {
+    id: "988b9826-dcc6-4b3c-afba-d6ec50300c82",
+    content: "study CCNA",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(3, "days")
+  },
+  {
+    id: "1216fe3b-15d0-4485-b3dd-0af4517677ef",
+    content: "study システム",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(3, "days")
+  },
+  {
+    id: "a94c1eee-3cca-4efb-a4ee-481f9106ce0a",
+    content: "study JAVA",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(3, "days")
+  },
+  {
+    id: "8561c028-4635-4b86-b22b-a01aa5e5ba15",
+    content: "read HTDP",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(4, "days")
+  },
+  {
+    id: "f401d5d0-9444-4cca-8fdb-39db14b521e8",
+    content: "create side project",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(4, "days")
+  },
+  {
+    id: "7d0ac165-f497-4808-8b95-d084d85b652a",
+    content: "create Java tutorial",
+    startTime: dayjs(),
+    category: "education",
+    interval: dayjs.duration(4, "days")
+  },
+  {
+    id: "e4e105a0-6bc4-4d09-934d-650dedce8eb8",
+    content: "backup computer",
+    startTime: dayjs(),
+    category: "maintenance",
+    interval: dayjs.duration(4, "months")
+  },
+  {
+    id: "253fcb2f-847f-420b-a271-6ef840739d2f",
+    content: "backup phones",
+    startTime: dayjs(),
+    category: "maintenance",
+    interval: dayjs.duration(4, "months")
   },
 ]
 
