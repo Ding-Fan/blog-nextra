8f22a7fb42cabcd4ce9d6e2477f787ba350778f7
Ding Fan <ouud@fastmail.com>
2024-10-03 07:16:37 +0900

    update

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function ct(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function isUrl(url: string): boolean {
+  try {
+    new URL(url); // Tries to create a new URL object
+    return true;
+  } catch (_) {
+    return false; // If it throws an error, it's not a valid URL
+  }
+}
