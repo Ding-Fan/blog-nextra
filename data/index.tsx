a367ea753f20e085cf78ee1a42a11aec15abc484
Ding Fan <ouud@fastmail.com>
2024-11-01 11:35:47 +0900

    Add guidance for disabling Vivaldi auto-update and create new Java subnetwork and simple calendar documentation

--- data/index.tsx
+++ data/index.tsx
@@ -2,6 +2,91 @@ import { ReactNode } from "react";
 import { getOssUrl } from "../scripts/utils";
 
 export const WORD_DATA = [
+  {
+    ruby: "よごす",
+    content: "汚す",
+  },
+  {
+    ruby: "きたない",
+    content: "汚い",
+  },
+  {
+    ruby: "ちょう",
+    content: "腸",
+  },
+  {
+    ruby: "ちょうかく",
+    content: "聴覚",
+  },
+  {
+    ruby: "ちょうこう",
+    content: "聴講",
+  },
+  {
+    ruby: "きく",
+    content: "聴く",
+  },
+  {
+    ruby: "どう",
+    content: "胴",
+  },
+  {
+    ruby: "にょう",
+    content: "尿",
+  },
+  {
+    ruby: "しにょう",
+    content: "し尿",
+  },
+  {
+    ruby: "にんしん",
+    content: "妊娠",
+  },
+  {
+    ruby: "はい",
+    content: "肺",
+  },
+  {
+    ruby: "ほっさ",
+    content: "発作",
+  },
+  {
+    ruby: "ほっそく、はっそく",
+    content: "発足",
+  },
+  {
+    ruby: "ひろう",
+    content: "疲労",
+  },
+  {
+    ruby: "つかれる",
+    content: "疲れる",
+  },
+  {
+    ruby: "かいぼう",
+    content: "解剖",
+  },
+  {
+    ruby: "ますい",
+    content: "麻酔",
+  },
+  {
+    ruby: "あさ",
+    content: "麻",
+  },
+  {
+    ruby: "よう",
+    content: "酔う",
+  },
+  {
+    ruby: "しょうもう",
+    content: "消耗",
+  },
+  {
+    ruby: "げり",
+    content: "下痢",
+  },
+
   { ruby: "おしむ", content: "惜しむ" },
   { ruby: "つつしむ", content: "謹む" },
   { ruby: "はばむ", content: "阻む" },
@@ -258,56 +343,67 @@ export const POSTS_DATA: Quote[] = [
   },
   {
     author: "Alan Perlis",
-    content: "A language that doesn't affect the way you think about programming is not worth knowing.",
+    content:
+      "A language that doesn't affect the way you think about programming is not worth knowing.",
     description: "",
     hidden: false,
   },
   {
     author: "Albert Einstein",
-    content: "The value of a college education is not the learning of many facts but the training of the mind to think",
+    content:
+      "The value of a college education is not the learning of many facts but the training of the mind to think",
     description: "",
     hidden: false,
   },
   {
     author: "Shakespeare",
-    content: <div>
-      Time doth transfix the flourish set on youth
-      <br />
-      And delves the parallels in beauty’s brow,
-      <br />
-      Feeds on the rarities of nature’s truth,
-      <br />
-      And nothing stands but for his scythe to mow:
-    </div>,
+    content: (
+      <div>
+        Time doth transfix the flourish set on youth
+        <br />
+        And delves the parallels in beauty’s brow,
+        <br />
+        Feeds on the rarities of nature’s truth,
+        <br />
+        And nothing stands but for his scythe to mow:
+      </div>
+    ),
     description: "Sonnet 60",
     hidden: false,
   },
   {
     author: "Warren Buffett",
-    content: "Only when the tide goes out do you discover who's been swimming naked.",
+    content:
+      "Only when the tide goes out do you discover who's been swimming naked.",
     description: "",
     hidden: false,
   },
   {
     author: "Why The Lucky Stiff",
-    content: "when you don't create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.",
+    content:
+      "when you don't create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.",
     description: null,
     hidden: false,
   },
   {
     author: "Albert Einstein",
-    content: "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution. It is, strictly speaking, a real factor in scientific research.",
+    content:
+      "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution. It is, strictly speaking, a real factor in scientific research.",
     description: "",
     hidden: false,
   },
   {
     author: "Chuck Palahniuk, Fight Club",
-    content:
+    content: (
       <div>
-        "Listen, now, you're going to die, Ray-mond K. K. K. Hessel, tonight. You might die in one second or in one hour, you decide. So lie to me. Tell me the first thing off the top of your head. Make something up. I don't give a shit. I have a gun.
+        "Listen, now, you're going to die, Ray-mond K. K. K. Hessel, tonight.
+        You might die in one second or in one hour, you decide. So lie to me.
+        Tell me the first thing off the top of your head. Make something up. I
+        don't give a shit. I have a gun.
         <br />
         <br />
-        Finally, you were listening and coming out of the little tragedy in your head.
+        Finally, you were listening and coming out of the little tragedy in your
+        head.
         <br />
         <br />
         Fill in the blank. What does Raymond Hessel want to be when he grows up?
@@ -316,7 +412,8 @@ export const POSTS_DATA: Quote[] = [
         Go home, you said you just wanted to go home, please.
         <br />
         <br />
-        No shit, I said. But after that, how did you want to spend your life? If you could do anything in the world.
+        No shit, I said. But after that, how did you want to spend your life? If
+        you could do anything in the world.
         <br />
         <br />
         Make something up.
@@ -334,26 +431,39 @@ export const POSTS_DATA: Quote[] = [
         A vet, you said. You want to be a vet, a veterinarian.
         <br />
         <br />
-        You could be in school working your ass off, Raymond Hessel, or you could be dead. You choose. I stuffed your wallet into the back of your jeans. So you really wanted to be an animal doctor. I took the saltwater muzzle of the gun off one cheek and pressed it against another. Is that what you've always wanted to be, Dr. Raymond K. K. K. K. Hessel, a veterinarian?...
+        You could be in school working your ass off, Raymond Hessel, or you
+        could be dead. You choose. I stuffed your wallet into the back of your
+        jeans. So you really wanted to be an animal doctor. I took the saltwater
+        muzzle of the gun off one cheek and pressed it against another. Is that
+        what you've always wanted to be, Dr. Raymond K. K. K. K. Hessel, a
+        veterinarian?...
         <br />
         <br />
-        So, I said, go back to school. If you wake up tomorrow morning, you find a way to get back into school.
+        So, I said, go back to school. If you wake up tomorrow morning, you find
+        a way to get back into school.
         <br />
         <br />
         I have your license.
         <br />
         <br />
-        I know who you are. I know where you live. I'm keeping your license, and I'm going to check on you, mister Raymond K. Hessel. In three months, and then six months, and then a year, and if you aren't back in school on your way to being a veterinarian, you will be dead...
+        I know who you are. I know where you live. I'm keeping your license, and
+        I'm going to check on you, mister Raymond K. Hessel. In three months,
+        and then six months, and then a year, and if you aren't back in school
+        on your way to being a veterinarian, you will be dead...
         <br />
         <br />
-        Raymond K. K. Hessel, your dinner is going to taste better than any meal you've ever eaten, and tomorrow will be the most beautiful day of your life."
-      </div>,
+        Raymond K. K. Hessel, your dinner is going to taste better than any meal
+        you've ever eaten, and tomorrow will be the most beautiful day of your
+        life."
+      </div>
+    ),
     description: null,
     hidden: true,
   },
   {
     author: "Alan Watts",
-    content: "The reason you want to be better is the reason why you aren't. We aren't better because we want to be. Because the road to hell is paved with good intentions.",
+    content:
+      "The reason you want to be better is the reason why you aren't. We aren't better because we want to be. Because the road to hell is paved with good intentions.",
     description: "The Road to Hell is Paved with Good Intentions",
     hidden: false,
   },
@@ -362,8 +472,8 @@ export const POSTS_DATA: Quote[] = [
     content: "自杀前请想一想，自杀是会下地狱的，而地狱里全是中国人。",
     description: null,
     hidden: true,
-  }
-]
+  },
+];
 
 export const PHOTOS_2024 = [
   {
@@ -408,7 +518,7 @@ export const HOMEPAGE_LINKS = [
   },
   {
     url: "https://bsky.app",
-    note: "Bluesky"
+    note: "Bluesky",
   },
   {
     url: "https://www.abaxlms.com",
