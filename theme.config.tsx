3aaff6a569ee83e3c46ab7c5c65ee9bc69054f37
Ding Fan <ouud@fastmail.com>
2025-01-03 00:02:33 +0900

    Add Chinese content recommendations with metadata and enhance theme configuration for SEO

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,9 +1,63 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
 import Footer from "./components/Footer";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>又不是不能写博客</span>,
+
+  head: function useHead() {
+    const config = useConfig()
+    const title = `${config.title} - 又不是不能写博客`
+    const description =
+      config.frontMatter.description || '又不是不能写博客'
+    const image = config.frontMatter.image || 'https://nextra.site/og.jpeg'
+    return (
+      <>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta name="og:image" content={image} />
+
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="apple-mobile-web-app-title" content="又不是不能写博客" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="https://dingfan.date" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </>
+    )
+  },
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
   // },
