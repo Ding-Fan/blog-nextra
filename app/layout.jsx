6cd88c11aa6c1cfce5b1afa89c6dc43b0c9e4258
Ding Fan <ouud@fastmail.com>
2025-02-23 09:06:19 +0900

    Refactor: Clean up layout component by removing unused imports and files; update metadata structure and footer implementation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,39 +1,34 @@
-import { Layout, Navbar, Footer } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
-import { getPageMap } from 'nextra/page-map'
+import { Layout, Navbar } from "nextra-theme-docs";
+import Footer from "../components/Footer";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
 // Required for theme styles, previously was imported under the hood
-import 'nextra-theme-docs/style.css'
- 
+import "nextra-theme-docs/style.css";
+
 export const metadata = {
   // ... your metadata API
   // https://nextjs.org/docs/app/building-your-application/optimizing/metadata
   title: {
-    default: '又不是不能写博客',
-    template: '%s | 又不是不能写博客'
+    default: "又不是不能写博客",
+    template: "%s | 又不是不能写博客",
   },
   openGraph: {
-    url: 'https://dingfan.date',
-    siteName: '又不是不能写博客',
-    locale: 'zh_CN',
-    type: 'website'
-  }
-}
+    url: "https://dingfan.date",
+    siteName: "又不是不能写博客",
+    locale: "zh_CN",
+    type: "website",
+  },
+};
 
- 
 // const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
-const footer = (
-  <Footer className="flex-col items-center md:items-start">
-    MIT {new Date().getFullYear()} © Nextra.
-  </Footer>
-)
 const navbar = (
-    <Navbar
-        logo={<span>又不是不能写博客</span>} 
-        // projectLink="https://github.com/shuding/nextra"
-    >
-        {/* <Search /> */}
-    </Navbar>
-)
+  <Navbar
+    logo={<span>又不是不能写博客</span>}
+    // projectLink="https://github.com/shuding/nextra"
+  >
+    {/* <Search /> */}
+  </Navbar>
+);
 export default async function RootLayout({ children }) {
   return (
     <html
@@ -46,25 +41,34 @@ export default async function RootLayout({ children }) {
     >
       <Head
         backgroundColor={{
-          dark: 'rgb(15, 23, 42)',
-          light: 'rgb(254, 252, 232)'
+          dark: "rgb(15, 23, 42)",
+          light: "rgb(254, 252, 232)",
         }}
         color={{
           hue: { dark: 120, light: 0 },
-          saturation: { dark: 100, light: 100 }
+          saturation: { dark: 100, light: 100 },
         }}
       >
         {/* Your additional tags should be passed as `children` of `<Head>` element */}
       </Head>
       <body>
         <Layout
-        //   banner={banner}
+          //   banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-        //   docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
-        //   editLink="Edit this page on GitHub"
-          sidebar={{ defaultMenuCollapseLevel: 1 }}
-          footer={footer}
+          //   docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          //   editLink="Edit this page on GitHub"
+          sidebar={{
+            toggleButton: true,
+            defaultMenuCollapseLevel: 1,
+          }}
+          editLink={null}
+          feedback={{ content: null, }}
+          nextThemes={{
+            forcedTheme: "light",
+          }}
+          darkMode={false}
+          footer={<Footer />}
 
           // ...Your additional theme config options
         >
@@ -72,5 +76,5 @@ export default async function RootLayout({ children }) {
         </Layout>
       </body>
     </html>
-  )
+  );
 }
